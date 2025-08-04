5afeffcca04db3b9d1b1f97f59b2819b85a7120d
codoundoye <niangalia05@gmail.com>
2025-08-04 01:05:44 +0000

    premier commit : projet calculatrice

---
+++ script.js
@@ -0,0 +1,72 @@
+
+let nombre1 = null;
+let affichage = "";
+let operation = null;
+let nombre2;
+let resultatfinal;
+
+const resultat = document.getElementById("resultat");
+const boutonChiffres = document.querySelectorAll(".chiffre");
+
+boutonChiffres.forEach(function (bouton) {
+    bouton.addEventListener("click", function () {
+        affichage += bouton.innerText;
+        resultat.innerText = affichage;
+    });
+});
+
+document.getElementById("plus").addEventListener("click", function () {
+    nombre1 = parseFloat(affichage);
+    operation = "+";
+    affichage = "";
+    resultat.innerText="+";
+});
+const moins=document.getElementById("moins");
+moins.addEventListener("click",function(){
+    nombre1 = parseFloat(affichage);
+    operation = "-";
+    affichage = "";
+    resultat.innerText="-";
+});
+const mult=document.getElementById("multiplier");
+mult.addEventListener("click",function(){
+    nombre1=parseFloat(affichage);
+    resultat.innerText="x";
+    operation="x";
+    affichage="";
+})
+const diviser=document.getElementById("diviser")
+diviser.addEventListener("click",function(){
+    nombre1=parseFloat(affichage);
+    resultat.innerText=":";
+    affichage="";
+    operation=":";
+})
+const effacer1=document.getElementById("clear");
+effacer1.addEventListener("click",function(){
+    resultat.innerText=0;
+})
+const effacer2=document.getElementById("clear");
+effacer2.addEventListener("click",function(){
+    resultat.innerText=resultat.innerText.slice(0,-1);
+})
+document.getElementById("egal").addEventListener("click", function () {
+    nombre2 = parseFloat(affichage);
+    resultatfinal=0;
+    if (operation === "+") {
+        resultatfinal = nombre1 + nombre2;
+    }
+    else if(operation==="-"){
+        resultatfinal=nombre1-nombre2;
+    }
+    else if(operation==="x"){
+        resultatfinal=nombre1*nombre2;
+    }
+    else if(operation=":"){
+        resultatfinal=nombre1/nombre2;
+    }
+
+    resultat.innerText = resultatfinal;
+    affichage = "";
+    operation = null;
+});
